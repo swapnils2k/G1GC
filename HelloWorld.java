a6e0a8fd44a09d2bb3fe32e5e6f2bdff607cc5e7
Rohit Joseph <rjoseph7@buffalo.edu>
2022-12-05 09:41:10 +0000

    Disabled logs

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,15 +2,14 @@ import java.util.*;
 public class HelloWorld {
 
     public static void main (String[] args) {
-        System.out.println("Hello World");
-        // int counter = 0;
-
-        // while(counter < 1) {
-        //     Person a = new Person(1);
-        //     System.out.println(a.getX());
-        //     counter++;
-        //     System.out.println(counter);
-        // }
+        // System.out.println("Hello World");
+        int counter = 0;
+        while(true) {
+            Person a = new Person(1);
+            // System.out.println(a.getX());
+            counter++;
+            // System.out.println(counter);
+        }
     }
 
 
